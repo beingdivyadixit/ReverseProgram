de34cb67be8bd561a7260156330d216b7ec4b387
Divya <beingdivyadixit@gmail.com>
2022-12-11 23:00:18 +0530

    Reverse Number Program

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the Number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            ReverseProgram.ReverseNumber(num);
+        }
+    }
+}
---
+++ ConsoleApp3/ReverseProgram.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class ReverseProgram
+    {
+        public static void ReverseNumber(int num)
+        {
+            int rem =0 , rev = 0;
+            while (num != 0)
+            {
+                rem = num % 10;
+                rev = (rev * 10) + rem;
+                num = num / 10;
+            }
+            Console.WriteLine("Reverse = "+rev);
+        }
+    }
+}
